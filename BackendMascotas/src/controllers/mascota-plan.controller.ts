3be0086557cc7cf7b37aaf2be1deec4e250607bd
carlosriobo4 <carlosriobo4@gmail.com>
2022-11-05 20:01:10 -0500

    Version S2C4 Web App MF

---
+++ BackendMascotas/src/controllers/mascota-plan.controller.ts
@@ -0,0 +1,37 @@
+import {
+  repository
+} from '@loopback/repository';
+import {
+  get,
+  getModelSchemaRef, param
+} from '@loopback/rest';
+import {
+  Mascota,
+  Plan
+} from '../models';
+import {MascotaRepository} from '../repositories';
+
+export class MascotaPlanController {
+  constructor(
+    @repository(MascotaRepository)
+    public mascotaRepository: MascotaRepository,
+  ) { }
+
+  @get('/mascotas/{id}/plan', {
+    responses: {
+      '200': {
+        description: 'Plan belonging to Mascota',
+        content: {
+          'application/json': {
+            schema: {type: 'array', items: getModelSchemaRef(Plan)},
+          },
+        },
+      },
+    },
+  })
+  async getPlan(
+    @param.path.string('id') id: typeof Mascota.prototype.id,
+  ): Promise<Plan> {
+    return this.mascotaRepository.plan(id);
+  }
+}
